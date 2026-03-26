0a736f923b0004b545f3eb3d1549b3319c0e70c9
xplorenow25-dotcom <xplorenow25@gmail.com>
2026-03-26 22:03:19 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -193,7 +193,7 @@ async function loadTicker() {
           <div class="ticker-item">
             <strong>${c.symbol}</strong>
             <span class="${isUp ? "green" : "red"}">
-              ${isUp ? "▲" : "▼"}
+              ${isUp ? "▲" : "▼"} $${price.toLocaleString()}
             </span>
             <span>•</span>
           </div>
